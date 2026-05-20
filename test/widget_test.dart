5b8cef713c218cb775daaea4383dfc42b1d101f0
poineko <eliteisan@gmail.com>
2026-05-20 15:03:52 +0700

    Add: screenshots

--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:cognitap/main.dart';
-
-void main() {
-  testWidgets('App launches smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const CogniTapApp());
-    expect(find.byType(CogniTapApp), findsOneWidget);
-  });
-}
